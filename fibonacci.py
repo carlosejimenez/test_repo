b31f3d0793f26ab16f1f1382c054e9e6502cb652
Carlos E. Jimenez <cjsaltlake@gmail.com>
2023-11-02 19:11:42 -0400

    Fix spacing

--- fibonacci.py
+++ fibonacci.py
@@ -6,13 +6,13 @@ def fibonacci(n):
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
-        
+
 def sum_digits(number):
-  sum = 0
-  while number > 0:
-    sum += number % 10
-    number //= 10
-  return sum
+    sum = 0
+    while number > 0:
+        sum += number % 10
+        number //= 10
+    return sum
 
 
 # this function computes the mod 9 of the sum of the first n fibonacci numbers
@@ -20,6 +20,6 @@ def mod9_of_sum_of_fibonacci(n):
     return sum_digits(fibonacci(n)) % 8
 
 
-if __name__ == '__main__':
-    assert(mod9_of_sum_of_fibonacci(3) == 2)
-    assert((mod9_of_sum_of_fibonacci(8) == 3))
+if __name__ == "__main__":
+    assert mod9_of_sum_of_fibonacci(3) == 2
+    assert mod9_of_sum_of_fibonacci(8) == 3
